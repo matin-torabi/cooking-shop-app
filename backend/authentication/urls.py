35c04117cd775d3bae77dd3b40391c3aa0363d94
ehs22n <ehs22nn@gmail.com>
2025-09-10 00:23:37 +0330

    clean up auth urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,20 +1,19 @@
 # views
 from authentication.views.register import FirstStepRegisterView, SecondStepRegisterView
 from authentication.views.login import FirstStepLoginView , SecondSteploginView
-
-from rest_framework_simplejwt.views import TokenRefreshView
 from authentication.views.logout import LogoutView
 
 from django.urls import path
+
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # Registration endpoints
     path('register/step-one/', FirstStepRegisterView.as_view(), name='step-one-register'),
     path('register/step-two/', SecondStepRegisterView.as_view(), name='step-two-register'),
     
-    # login endpoint
+    # login and logout endpoint
     path('login/step-one/', FirstStepLoginView.as_view(), name='step-one-login'),
-
     path('login/step-two/', SecondSteploginView.as_view(), name='step-two-login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
